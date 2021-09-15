5f9a6b6a8afc98d1f0c1a276dd2a19048db13438
mohammedrehan275 <65021509+mohammedrehan275@users.noreply.github.com>
2021-09-15 14:10:12 +0530

    adding landmark screen

---
+++ lib/screens/landmark_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LandmarkScreen extends StatefulWidget {
+  const LandmarkScreen({Key? key}) : super(key: key);
+
+  @override
+  _LandmarkScreenState createState() => _LandmarkScreenState();
+}
+
+class _LandmarkScreenState extends State<LandmarkScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/widgets/top_picks.dart
+++ lib/widgets/top_picks.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:landmark_finder/screens/landmark_screen.dart';
 
 class TopPicks extends StatelessWidget {
   const TopPicks({Key? key}) : super(key: key);
@@ -40,67 +41,73 @@ class TopPicks extends StatelessWidget {
         child: Container(
           height: 340.0,
           child: ListView(scrollDirection: Axis.horizontal, children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0.0, 15.0, 15.0, 15.0),
-              child: Container(
-                width: 180.0,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20.0),
-                  color: Colors.white,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.blue.shade50,
-                      blurRadius: 25.0, // soften the shadow
-                      spreadRadius: 5.0, //extend the shadow
-                      offset: Offset(
-                        15.0, // Move to right  horizontally
-                        3.0, // Move to bottom Vertically
-                      ),
-                    )
-                  ],
-                ),
-                child: Column(children: <Widget>[
-                  ClipRRect(
+            GestureDetector(
+              onTap: () => Navigator.push(
+                context,
+                MaterialPageRoute(builder: (_) => LandmarkScreen()),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(0.0, 15.0, 15.0, 15.0),
+                child: Container(
+                  width: 180.0,
+                  decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(20.0),
-                    child: Image(
-                      height: 200.0,
-                      width: 180.0,
-                      image: NetworkImage(
-                          'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  SizedBox(height: 10.0),
-                  Padding(
-                    padding: const EdgeInsets.all(12.0),
-                    child: Column(
-                      children: [
-                        Text('The National Art Center',
-                            style: TextStyle(
-                              fontSize: 18.0,
-                            )),
-                        SizedBox(
-                          height: 12.0,
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.blue.shade50,
+                        blurRadius: 25.0, // soften the shadow
+                        spreadRadius: 5.0, //extend the shadow
+                        offset: Offset(
+                          15.0, // Move to right  horizontally
+                          3.0, // Move to bottom Vertically
                         ),
-                        Row(children: <Widget>[
-                          Icon(
-                            Icons.location_on_outlined,
-                            size: 17.0,
-                            color: Colors.blue[400],
-                          ),
-                          SizedBox(
-                            width: 10.0,
-                          ),
-                          Text('Minato',
-                              style: TextStyle(
-                                fontSize: 17.0,
-                                color: Colors.grey,
-                              )),
-                        ])
-                      ],
-                    ),
+                      )
+                    ],
                   ),
-                ]),
+                  child: Column(children: <Widget>[
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(20.0),
+                      child: Image(
+                        height: 200.0,
+                        width: 180.0,
+                        image: NetworkImage(
+                            'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    SizedBox(height: 10.0),
+                    Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Column(
+                        children: [
+                          Text('The National Art Center',
+                              style: TextStyle(
+                                fontSize: 18.0,
+                              )),
+                          SizedBox(
+                            height: 12.0,
+                          ),
+                          Row(children: <Widget>[
+                            Icon(
+                              Icons.location_on_outlined,
+                              size: 17.0,
+                              color: Colors.blue[400],
+                            ),
+                            SizedBox(
+                              width: 10.0,
+                            ),
+                            Text('Minato',
+                                style: TextStyle(
+                                  fontSize: 17.0,
+                                  color: Colors.grey,
+                                )),
+                          ])
+                        ],
+                      ),
+                    ),
+                  ]),
+                ),
               ),
             ),
             Padding(
